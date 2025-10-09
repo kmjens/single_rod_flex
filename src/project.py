c9d54291a9b4d79d51017edcb805072878321bb3
Kate Jensen <kmjens@umich.edu>
2025-10-09 14:52:25 -0400

    fix mesh_obj in dynamical bonding if statement

--- src/project.py
+++ src/project.py
@@ -211,10 +211,10 @@ def Run_implementation(job, communicator):
     print('k_area set to: ', k_area)
     
     # Dynamical bonding updater:
-    if SP.dynamic_bonding == 'True':
+    if SP.dynamical_bonding == 'True':
         mesh_updater = hoomd.md.update.MeshDynamicalBonding(
                         trigger = hoomd.trigger.Periodic(100),
-                        mesh=mesh,
+                        mesh=mesh_obj,
                         kT=kT,
                         forces=[mesh_bond_potential, helfrich_potential])
 
